4e8cf0878577590fe84ff4c8bba5c13297d25d8d
bar haim <barha@il.ibm.com>
2022-10-01 22:20:28 +0300

    init

---
+++ js/service.js
@@ -0,0 +1,6 @@
+var SERVICE_URLS = {
+    current: 'http://192.168.1.91:4000/current',
+    history: 'http://192.168.1.91:4000/v2/positions?ts=',
+    visibility: 'http://192.168.1.91:4000/visibility',
+    groundStations: 'http://192.168.1.91:4000/ground-stations'
+}
